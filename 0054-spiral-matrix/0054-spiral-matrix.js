96c091fb99b1589243e7b9ddefaa27fd204cfcf2
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2024-01-29 22:51:35 +0530

    Time: 55 ms (34.70%), Space: 48.9 MB (6.32%) - LeetHub

---
+++ 0054-spiral-matrix/0054-spiral-matrix.js
@@ -0,0 +1,46 @@
+/**
+ * @param {number[][]} matrix
+ * @return {number[]}
+ */
+var spiralOrder = function(mat) {
+     let ans = [];
+
+  // Determine the number of rows and columns
+  let n = mat.length; // no. of rows
+  let m = mat[0].length; // no. of columns
+
+  // Initialize the pointers reqd for traversal.
+  let top = 0, left = 0, bottom = n - 1, right = m - 1;
+
+  // Loop until all elements are not traversed.
+  while (top <= bottom && left <= right) {
+    // For moving left to right
+    for (let i = left; i <= right; i++)
+      ans.push(mat[top][i]);
+
+    top++;
+
+    // For moving top to bottom.
+    for (let i = top; i <= bottom; i++)
+      ans.push(mat[i][right]);
+
+    right--;
+
+    // For moving right to left.
+    if (top <= bottom) {
+      for (let i = right; i >= left; i--)
+       ans.push(mat[bottom][i]);
+
+      bottom--;
+    }
+
+    // For moving bottom to top.
+    if (left <= right) {
+      for (let i = bottom; i >= top; i--)
+        ans.push(mat[i][left]);
+
+      left++;
+    }
+  }
+  return ans;
+};
