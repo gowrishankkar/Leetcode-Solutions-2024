e6be9311a8456e817f51a36de57409cec6144a0c
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2024-02-25 00:42:47 +0530

    Time: 59 ms (49.11%), Space: 51.6 MB (22.30%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.js
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.js
@@ -11,52 +11,30 @@
  * @return {number[][]}
  */
 var zigzagLevelOrder = function(root) {
-    if(!root) {
-        return []
-    }
-    let arr = [root]
-    let res = []
-    let flag = true
-    while(arr.length) {
-        if(flag) {
-            let i = 0
-            let size = arr.length
-            let temp = []
+    if(!root) return []
 
-            while(i < size) {
-                let curr = arr.shift()
-                temp.push(curr.val)
+    const result = []
+    const queue = [root]
+    let flip = false
 
-                if(curr.left) {
-                    arr.push(curr.left)
-                }
-                 if(curr.right) {
-                    arr.push(curr.right)
-                }
-                i++
-            }
-            res.push(temp)
+    while(queue.length){
+        const currentLevel = []
+        const queueLength = queue.length
+
+        for(let i=0; i<queueLength; i++){
+            const currentNode = queue.shift()
+
+            flip ? currentLevel.unshift(currentNode.val) : currentLevel.push(currentNode.val)
+
+            if(currentNode.left) queue.push(currentNode.left)
+            if(currentNode.right) queue.push(currentNode.right)
         }
-        else {
-            let i = 0
-            let size = arr.length
-            let temp = []
 
-            while(i < size) {
-                let curr = arr.pop()
-                temp.push(curr.val)
+        result.push(currentLevel)
+        flip = !flip
 
-                if(curr.right) {
-                    arr.unshift(curr.right)
-                }
-                 if(curr.left) {
-                    arr.unshift(curr.left)
-                }
-                i++
-            }
-            res.push(temp)
-        }
-        flag = !flag
+       
     }
-    return res
+    
+    return result
 };
